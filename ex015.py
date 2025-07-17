42a140857998265961f16037eb66813c270454aa
nalugao <nalugao@hotmail.com>
2025-07-17 15:13:30 -0300

    new exercises done: temperature converter

---
+++ ex015.py
@@ -0,0 +1,2 @@
+# Create a program that asks for the number of kilometers driven by a rented car and the number of days it was rented.
+# Calculate the total price to pay, knowing that the car costs R$60 per day and R$0.15 per kilometer.
