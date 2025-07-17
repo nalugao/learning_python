f085043ad55e8591eea086a5b9c5148f200f7122
nalugao <nalugao@hotmail.com>
2025-07-17 15:25:38 -0300

    new exercise done

--- ex015.py
+++ ex015.py
@@ -1,2 +1,14 @@
 # Create a program that asks for the number of kilometers driven by a rented car and the number of days it was rented.
-# Calculate the total price to pay, knowing that the car costs R$60 per day and R$0.15 per kilometer.
+# Calculate the total price to pay, knowing that the car costs R$60 per day and R$0.15 per kilometer driven.
+
+km = float(input('How many kilometers did you drive? '))
+days = int(input('How many days did you rent the car? '))
+
+km_cost = km*0.15
+day_cost = days*60
+total = km_cost+day_cost
+
+#or
+#total = (km*0.15) + (days*60)
+
+print(f'The total to pay is R$ {total:.2f}')
