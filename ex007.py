eff9cf35be7c3cfacc3e165518d091b0e9dc18ae
nalugao <nalugao@hotmail.com>
2025-07-17 11:51:54 -0300

    new exercises done: practicing arithmetic operations

--- ex007.py
+++ ex007.py
@@ -1,8 +1,8 @@
 # Develop a program that reads two grades of a student, calculates, and shows their average.
 
-g1 = int(input('Write the first grade: '))
-g2 = int(input('Write the second grade: '))
+g1 = float(input('Write the first grade: '))
+g2 = float(input('Write the second grade: '))
 
 a = (g1+g2)/2
 
-print(f'The average os the semester is {a}')
+print(f'The average os the semester is {a:.2f}')
