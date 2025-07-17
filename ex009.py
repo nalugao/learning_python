93cb09f35896618f0b436d208051ae3a6d906a98
nalugao <nalugao@hotmail.com>
2025-07-17 12:05:43 -0300

    new exercises done: practicing arithmetic operations

--- ex009.py
+++ ex009.py
@@ -1,11 +1,13 @@
 # Write a program that reads any integer and shows its multiplication table on the screen.
 
+#Using while
 n = int(input('Write a number: '))
 
 print(f'The multiplication table of the number {n} is:')
-
+print("-"*12)
 cont = 0
 while cont <= 10:
     t = n*cont
-    print(t)
+    print(f' {n} x {cont} = {t} ')
     cont += 1
+print("-"*12)
