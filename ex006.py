ebb07c6a019138e20b34be3c0dcffb4a410ab7a0
nalugao <nalugao@hotmail.com>
2025-07-17 11:49:02 -0300

    new exercises done: practicing arithmetic operations

--- ex006.py
+++ ex006.py
@@ -1,8 +1,8 @@
 # Write a program that reads a number and displays its double, triple, and square root.
 
 n = int(input('Write a number: '))
-d = n**2
-t = n**3
+d = n*2
+t = n*3
 sr = n**(1/2)
 
 print(f'The double of the number {n} is {d}, the triple is {t} and the square root is {sr:.2f}.')
