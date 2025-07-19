60c92b07fc724c490552296fa081db097fc649a0
nalugao <nalugao@hotmail.com>
2025-07-19 12:19:36 -0300

    a program that plays a mp3's file

---
+++ ex021.py
@@ -0,0 +1,7 @@
+# faça um programa que abra e reproduza o audio de um arquivo MP3.
+
+import pygame
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
